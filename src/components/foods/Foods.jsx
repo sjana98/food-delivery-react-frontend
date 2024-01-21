562b8ab38639a605dfa1e7ad73a8dbb3b3cee19f
sjana98 <sjana8901@gmail.com>
2024-01-21 21:02:51 +0530

    Foods type section of home page add

--- src/components/foods/Foods.jsx
+++ src/components/foods/Foods.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import classes from "./foods.module.css";
+import { Link } from "react-router-dom";
+import { foodTypes } from '../../data/data';
 
 function Foods() {
   return (
-    <div>Foods</div>
+    <>
+      <section id='foods' className={classes.container}>
+        <div className={classes.wrapper}>
+          <span className={classes.subtitle}>What we offer!</span>
+          <h2 className={classes.title}>Home kitchen crafts perfect meals for you</h2>
+          <div className={classes.foods}>
+            {foodTypes.map((foodtype) => (
+              <Link to={`$/foods/${foodtype.name}`} key={foodtype.id}>
+                <div className={classes.foodtypeName}>
+                  <h4>{foodtype.name}</h4>
+                </div>
+                <div className={classes.imgContainer}>
+                  <img src={foodtype.img} alt="foodtype_img" />
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
