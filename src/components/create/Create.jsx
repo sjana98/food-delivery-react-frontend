cbbd21c5ade85803d73633189149f68f119c9601
sjana98 <sjana8901@gmail.com>
2024-02-02 17:14:07 +0530

    create product ui done

--- src/components/create/Create.jsx
+++ src/components/create/Create.jsx
@@ -3,7 +3,38 @@ import classes from "./create.module.css";
 
 function Create() {
   return (
-    <div>Create</div>
+    <>
+      <div className={classes.container}>
+        <div className={classes.wrapper}>
+          <h2 className={classes.title}>Add food</h2>
+          <form action="" encType='multiple/form-data'>
+            <div className={classes.inputWrapper}>
+              <label htmlFor="title">Title:</label>
+              <input type="text" id = "title" placeholder='Title...' className={ classes.input} />
+            </div>
+            <div className={classes.inputWrapper}>
+              <label htmlFor="description">Description:</label>
+              <textarea id = "description" placeholder='Description...' className={ classes.input} />
+            </div>
+            <div className={classes.inputWrapper}>
+              <label htmlFor="category">Category:</label>
+              <input type="text" id = "category" placeholder='Category...' className={ classes.input} />
+            </div>
+            <div className={classes.inputWrapper}>
+              <label htmlFor="image" className={classes.fileLabelInput}>Image: <span>Upload here</span></label>
+              <input style={{ display: "none" }} type="file" id='image' placeholder='Image...' className={classes.input} />
+            </div>
+            <div className={classes.inputWrapper}>
+              <label htmlFor="price">Price:</label>
+              <input type="number" id = "price" step={0.01} placeholder='Price...' className={ classes.input } />
+            </div>
+            <div className={classes.submitBtnWrp}>
+              <button type='Submit' className={classes.SubBtn}>Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
