cd8a0c1ecf17ec2c2cba4057fad136d227eb71c8
sjana98 <sjana8901@gmail.com>
2024-02-04 21:19:53 +0530

    redux store code modify

--- src/reduxToolkit/store.js
+++ src/reduxToolkit/store.js
@@ -1,22 +1,28 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import authReducer from "./authSlice";
-
+import authSlice from "./authSlice";
 
 const persistConfig = {
   key: "auth",
+  version: 1,
   storage,
 };
 
-const persistedReducer = persistReducer(persistConfig, authReducer);
+const reducers = combineReducers({
+  auth: authSlice,
+})
 
-const rootReducer = combineReducers({
-  auth: persistedReducer,
-});
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export const persistor = persistStore(store);
+export let persistor = persistStore(store);
