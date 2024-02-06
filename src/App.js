ff25b7e5b06b6a794fb9e1daf339d5b04f0c5a5d
sjana98 <sjana8901@gmail.com>
2024-02-06 20:56:33 +0530

    file modify according to window scroll

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
-import Login from './components/login/Login';
-import Signup from './components/signup/Signup';
-import Create from './components/create/Create';
-import FoodDetails from './components/foodDetails/FoodDetails';
-import FoodCatalog from './components/foodCatalog/FoodCatalog';
-import Cart from './components/cart/Cart';
-import Checkout from './components/checkout/Checkout';
-import LoginPassword from './components/loginPasswordChange/LoginPassword';
-import RoutesControll from './components/RoutesControll/RoutesControll';
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
+import Create from "./components/create/Create";
+import FoodDetails from "./components/foodDetails/FoodDetails";
+import FoodCatalog from "./components/foodCatalog/FoodCatalog";
+import Cart from "./components/cart/Cart";
+import Checkout from "./components/checkout/Checkout";
+import LoginPassword from "./components/loginPasswordChange/LoginPassword";
+import RoutesControll from "./components/RoutesControll/RoutesControll";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/Login' element={<Login />} />
-          <Route path='/Signup' element={<Signup />} />
-          <Route path='/passwordChange' element={<LoginPassword />} />
-          
-          <Route element={<RoutesControll/>}>
-            <Route path='/' element={<Home />} />
-            <Route path='/create' element={<Create />} />
-            <Route path='/food/:id' element={<FoodDetails />} />
-            <Route path='/foods/:id' element={<FoodCatalog />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/checkout' element={<Checkout />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/passwordChange" element={<LoginPassword />} />
+
+        <Route element={<RoutesControll />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/food/:id" element={<FoodDetails />} />
+          <Route path="/foods/:name" element={<FoodCatalog />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
 
       <Footer />
     </div>
   );
 }
 
-export default App;
+function AppWithRouter() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default AppWithRouter;
