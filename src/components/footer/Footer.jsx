b24f19e55f56bed6cc07dbcf07b73c7a6b9a4694
sjana98 <sjana8901@gmail.com>
2024-02-23 21:02:27 +0530

    footer ui decoration

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import classes from "./footer.module.css";
 import { AiTwotonePhone, AiTwotoneMail, AiFillFacebook, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { BsTwitterX } from 'react-icons/bs';
+import { GiHamburger } from "react-icons/gi";
+import { FaPizzaSlice } from "react-icons/fa";
+import { CiPizza } from "react-icons/ci";
+import { GiFullPizza } from "react-icons/gi";
+import { LiaHamburgerSolid } from "react-icons/lia";
+
 
 function Footer() {
   return (
     <>
       <div className={classes.container}>
+        {/* Decoration area */}
+        <CiPizza className={classes.pizzaIcon1} />
+        <FaPizzaSlice className={classes.pizzaIcon2} />
+        <GiFullPizza className={classes.pizzaIcon3} />
+        <GiHamburger className={classes.burgerIcon1} />
+        <LiaHamburgerSolid className={classes.burgerIcon2} />
+        <LiaHamburgerSolid className={classes.burgerIcon3} />
+        <GiHamburger className={classes.burgerIcon4} />
+        {/* Decoration area end*/}
+        
         <div className={classes.wrapper}>
           <div className={classes.col}>
             <div className={classes.title}>Working Time</div>
